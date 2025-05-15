dd336ddec51144658174ea067127b8a4705f245d
Peter Rebrun <brun65i@gmail.com>
2025-05-15 22:50:03 +0930

    Added '--force' option to remove dirty worktree, other sanity checks

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import argparse
 import logging
 import os
+import re
 import subprocess as sp
 import sys
 from pathlib import Path
@@ -21,9 +22,30 @@ def path_is_truthy(path: Path) -> bool:
 def git(*cmds: str, cwd: Optional[Path] = None) -> sp.CompletedProcess:
     if not cwd:
         cwd = Path.cwd()
-    git_command = [which("git"), *cmds]
+    git_command = list(filter(None, [which("git"), *cmds]))
     proc = sp.run(git_command, capture_output=True, cwd=cwd, text=True, check=True)
     return proc
+
+
+def is_inside_git_repo() -> bool:
+    try:
+        git("rev-parse")
+    except sp.CalledProcessError:
+        return False
+    return True
+
+
+def get_default_branch_name() -> str:
+    try:
+        result = git("remote", "show", "origin")
+    except sp.CalledProcessError:
+        return ""
+    origin_regex = re.compile(r"^\s*HEAD\s*branch:\s*(.*)$")
+    for line in result.stdout.split("\n"):
+        match = re.match(origin_regex, line)
+        if match:
+            return match.group(1)
+    return ""
 
 
 def list_worktrees() -> str:
@@ -39,12 +61,17 @@ def get_bare_worktree_path() -> Path:
 
 def add_worktree(args: argparse.Namespace) -> Path:
     bare_path = get_bare_worktree_path()
-    git("worktree", "add", *args.path, cwd=bare_path)
     worktree_path = bare_path / args.path[0]
-    logging.info("Fetching changes")
-    git("fetch", "--all", cwd=worktree_path)
-    logging.info("Merging with master")
-    git("merge", "master", cwd=worktree_path)
+    try:
+        git("worktree", "add", *args.path, cwd=bare_path)
+        default_branch_name = get_default_branch_name()
+        if default_branch_name:
+            logging.info("Fetching changes")
+            git("fetch", "--all", cwd=worktree_path)
+            logging.info(f"Merging with {default_branch_name}")
+            git("merge", default_branch_name, cwd=worktree_path)
+    except sp.CalledProcessError:
+        logging.exception(f"Failed to add {args.path[0]} worktree", exc_info=True)
     return worktree_path
 
 
@@ -61,7 +88,6 @@ def select_worktree(exclude_bare: bool = True) -> Path:
         return Path()
 
 
-# TODO: check if there are modified or untracked files before removing
 def remove_worktree(args: argparse.Namespace) -> Path:
     bare_path = get_bare_worktree_path()
     if args.all:
@@ -73,7 +99,7 @@ def remove_worktree(args: argparse.Namespace) -> Path:
             dir_path = line.split()[0]
             try:
                 logging.info(f"Removing {dir_path}...")
-                git("worktree", "remove", dir_path)
+                git("worktree", "remove", dir_path, "--force" if args.force else "")
             except sp.CalledProcessError:
                 logging.exception(
                     f"Failed to remove {dir_path} worktree", exc_info=True
@@ -84,7 +110,15 @@ def remove_worktree(args: argparse.Namespace) -> Path:
         cwd_path = Path.cwd()
         if path_is_truthy(dir_path):
             logging.info(f"Removing worktree: {dir_path}")
-            git("worktree", "remove", str(dir_path))
+            try:
+                git(
+                    "worktree", "remove", str(dir_path), "--force" if args.force else ""
+                )
+            except sp.CalledProcessError:
+                logging.exception(
+                    f"Failed to remove {dir_path} worktree", exc_info=True
+                )
+                return Path()
             return bare_path if dir_path.name == cwd_path.name else cwd_path
         return Path()
 
@@ -116,10 +150,19 @@ def main() -> None:
 
     parser_remove = subparsers.add_parser("remove", help="Remove worktree(s)")
     parser_remove.add_argument(
-        "--all", action="store_true", help="Remove all non-bare, non-master worktrees"
+        "--all", action="store_true", help="Remove all worktrees"
+    )
+    parser_remove.add_argument(
+        "--force",
+        action="store_true",
+        help="Force removing worktrees with modified or untracked files",
     )
 
     args = parser.parse_args()
+
+    if not is_inside_git_repo():
+        logging.error("Not a git repository")
+        sys.exit(128)
 
     if args.command == "list":
         print(list_worktrees(), file=sys.stderr)
